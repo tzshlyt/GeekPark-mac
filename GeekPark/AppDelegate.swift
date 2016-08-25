9bf0a853689bb8fb67328e1abd0fcaedcca3fa7b
lanyutao <lanyutaoem@163.com>
2016-08-25 16:42:22 +0800

    init project

---
+++ GeekPark/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GeekPark
+//
+//  Created by lan on 16/8/25.
+//  Copyright © 2016年 lan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
