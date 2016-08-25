158b81498dab5c4772301638bdc1fc2b1c324753
lanyutao <lanyutaoem@163.com>
2016-08-25 23:52:05 +0800

    import Alamofire

---
+++ GeekPark/Network/HttpHelper.swift
@@ -0,0 +1,14 @@
+//
+//  HttpHelper.swift
+//  GeekPark
+//
+//  Created by lan on 16/8/25.
+//  Copyright © 2016年 lan. All rights reserved.
+//
+
+import Cocoa
+import Alamofire
+
+class HttpHelper: NSObject {
+
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+
+target 'GeekPark' do
+    pod 'Alamofire', '3.4.1'
+end
