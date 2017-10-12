2a62a757a4123c0c113952af5ac183fe889d8574
xiaopengs <xiaopengscom@gmail.com>
2017-10-12 23:01:49 +0800

    修改使用SwiftyJson获取接口信息

--- GeekPark/Network/HttpHelper.swift
+++ GeekPark/Network/HttpHelper.swift
@@ -3,47 +3,41 @@
 //  GeekPark
 //
 //  Created by lan on 16/8/25.
+//  Modified by Xiaopeng
 //  Copyright © 2016年 lan. All rights reserved.
 //
 
 import Cocoa
 import Alamofire
-import Fuzi
+import SwiftyJSON
 
 class HttpHelper: NSObject {
-    let url = "http://www.geekpark.net"
-
+    let url = "http://main_test.geekpark.net/api/v1/?page=1"
+    
     func getNews(_ handle: @escaping (_ models: [GPModel]) -> ()) {
-        Alamofire.request(url).responseString { response in
-            let html = response.result.value
-            
-            do {
-                var models = [GPModel]()
-                let doc = try HTMLDocument(string: html!, encoding: String.Encoding.utf8)
-                
-                for article in doc.xpath("//article[@class='article-item']") {
-                    var title = String()
-                    var imgUrl = String()
-                    var category = String()
-                    var time = String()
-                    var href = String()
+        
+        Alamofire.request(url).validate().responseJSON { response in
+            guard let value = response.result.value else { return }
+            let json = JSON(value)
+            guard let homepage_posts  = json["homepage_posts"].array else { return }
+            var models = [GPModel]()
+            for homepage_post in homepage_posts {
+                guard let data  = homepage_post["data"].array else { return }
+                for post in data {
                     
-                    if let imgEle = article.firstChild(xpath:".//a/div/img") {
-                        imgUrl = imgEle["data-src"]!
-                    }
+                    guard let title  = post["post"]["title"].string else { return }
+                    guard let imgUrl  = post["post"]["cover_url"].string else { return }
+                    guard let category  = post["post"]["column"]["title"].string else { return }
+                    guard let published_timestamp  = post["post"]["published_timestamp"].int else { return }
+                    guard let id  = post["post"]["id"].int else { return }
                     
-                    if let categoryEle =  article.firstChild(xpath: ".//div/div/a[1]") {
-                        category = categoryEle.stringValue
-                    }
+                    let href = "http://www.geekpark.net/news/\(id)"
+                    let timeInterval: TimeInterval = TimeInterval(published_timestamp)
+                    let date = Date(timeIntervalSince1970: timeInterval)
+                    let dformatter = DateFormatter()
+                    dformatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                    let time = dformatter.string(from: date)
                     
-                    if let titleEle = article.firstChild(xpath:".//div/div/a[2]") {
-                        title = titleEle["data-event-label"]!
-                        href = self.url + titleEle["href"]!
-                    }
-                    
-                    if let timeEle = article.firstChild(xpath: ".//div/div/div/div/span[2]/a") {
-                        time = timeEle["title"]!
-                    }
                     
                     let model = GPModel(
                         title: title,
@@ -52,14 +46,11 @@ class HttpHelper: NSObject {
                         time: time,
                         href: href
                     )
-                    
                     models.append(model)
                 }
-                handle(models)
-            }catch let error {
-                print(error)
             }
-
+            handle(models)
         }
     }
 }
+
--- Podfile
+++ Podfile
@@ -3,6 +3,6 @@ use_frameworks!
 
 target 'GeekPark' do
     pod 'Alamofire', '4.5.0' 
-    pod 'Fuzi', '1.0.1'
+    pod 'SwiftyJSON', '3.1.4'
     pod 'Kingfisher', '3.10.2'
 end
