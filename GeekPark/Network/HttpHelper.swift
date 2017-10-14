5992753acfd8f9348779a7e027792e2d990309aa
xiaopengs <xiaopengscom@gmail.com>
2017-10-14 12:51:23 +0800

    调整下HttpHelper.swift

--- GeekPark/Network/HttpHelper.swift
+++ GeekPark/Network/HttpHelper.swift
@@ -3,7 +3,6 @@
 //  GeekPark
 //
 //  Created by lan on 16/8/25.
-//  Modified by Xiaopeng
 //  Copyright © 2016年 lan. All rights reserved.
 //
 
@@ -16,41 +15,47 @@ class HttpHelper: NSObject {
     
     func getNews(_ handle: @escaping (_ models: [GPModel]) -> ()) {
         
-        Alamofire.request(url).validate().responseJSON { response in
-            guard let value = response.result.value else { return }
-            let json = JSON(value)
-            guard let homepage_posts  = json["homepage_posts"].array else { return }
-            var models = [GPModel]()
-            for homepage_post in homepage_posts {
-                guard let data  = homepage_post["data"].array else { return }
-                for post in data {
-                    
-                    guard let title  = post["post"]["title"].string else { return }
-                    guard let imgUrl  = post["post"]["cover_url"].string else { return }
-                    guard let category  = post["post"]["column"]["title"].string else { return }
-                    guard let published_timestamp  = post["post"]["published_timestamp"].int else { return }
-                    guard let id  = post["post"]["id"].int else { return }
-                    
-                    let href = "http://www.geekpark.net/news/\(id)"
-                    let timeInterval: TimeInterval = TimeInterval(published_timestamp)
-                    let date = Date(timeIntervalSince1970: timeInterval)
-                    let dformatter = DateFormatter()
-                    dformatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-                    let time = dformatter.string(from: date)
-                    
-                    
-                    let model = GPModel(
-                        title: title,
-                        imgUrl: imgUrl,
-                        category: category,
-                        time: time,
-                        href: href
-                    )
-                    models.append(model)
+        Alamofire.request(url, method: .get).responseJSON { response in
+            switch response.result {
+            case .failure(let error):
+                print("获取接口数据出错",error)
+            case .success:
+                print("Load data success")
+                
+                var models = [GPModel]()
+                if let value = response.result.value {
+                    let json = JSON(value), results = json["homepage_posts"]
+                    for result in results.arrayValue {
+                        let datas = result["data"]
+                        for data in datas.arrayValue {
+                            
+                            guard let title = data["post"]["title"].string else { return }
+                            guard let imgUrl = data["post"]["cover_url"].string else { return }
+                            guard let category  = data["post"]["column"]["title"].string else { return }
+                            guard let published_timestamp  = data["post"]["published_timestamp"].int else { return }
+                            guard let id = data["post"]["id"].int else { return }
+                            let href  = "http://www.geekpark.net/news/\(id)"
+                            
+                            let timeInterval: TimeInterval = TimeInterval(published_timestamp)
+                            let date = Date(timeIntervalSince1970: timeInterval)
+                            let dformatter = DateFormatter()
+                            dformatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                            let time = dformatter.string(from: date)
+                            
+                            let model = GPModel(
+                                title: title,
+                                imgUrl: imgUrl,
+                                category: category,
+                                time: time,
+                                href: href
+                            )
+                            models.append(model)
+                        }
+                    }
+                    handle(models)
                 }
             }
-            handle(models)
         }
+        
     }
 }
-
